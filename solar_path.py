fcb48115f4560a9c892ffaa9560529ccfa209fb2
srlm-io <srlm@srlm.io>
2018-02-25 11:39:53 -0500

    initial commit

---
+++ solar_path.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+
+import math
+from datetime import datetime
+
+import pytz
+from PIL import Image, ImageDraw, ImageFont
+from pysolar import solar, radiation
+
+tz = pytz.timezone("US/Pacific")
+
+position = [48.06288, -118.1776]  # shopside
+
+dates = [
+    [2017, 12, 21],  # shortest day of the year
+    [2018, 6, 21]  # longest day of the year
+]
+
+im = Image.open("shopside_working.jpg")
+
+center_x = 1629
+center_y = 1193
+
+deg_15 = 3041
+deg_10 = 3114
+
+radius = 10
+
+draw = ImageDraw.Draw(im)
+draw.ellipse((center_x - radius, center_y - radius, center_x + radius, center_y + radius), fill=128)
+
+
+def calculateDistance(elevation):
+    deg_15_pixels = deg_15 - center_x
+    deg_10_pixels = deg_10 - center_x
+
+    m = (deg_15_pixels - deg_10_pixels) / 5
+
+    b = deg_15_pixels - m * 15
+
+    return m * elevation + b
+
+
+def calculateX(distance_pixel, azimuth):
+    return math.cos((180 - azimuth) * math.pi / 180) * distance_pixel
+
+
+def calculateY(distance_pixel, azimuth):
+    return -math.sin((180 - azimuth) * math.pi / 180) * distance_pixel
+
+
+font = ImageFont.truetype("Waree.ttf", 16)
+
+for date in dates:
+    for hour in range(0, 24):
+        for minute in [0, 15, 30, 45]:
+            sample_time = tz.localize(datetime(*date, hour, minute, 0), is_dst=None)
+            elevation = solar.get_altitude(*position, sample_time)
+            # Move the 0 point to the north
+            azimuth = (540 - solar.get_azimuth(*position, sample_time)) % 360
+            radiation_w_m2 = radiation.get_radiation_direct(sample_time, elevation)
+            print('{:10} {:20} {:20} {}'.format(sample_time.strftime('%H:%M %Z'), elevation, azimuth, radiation_w_m2))
+
+            distance_pixel = calculateDistance(elevation)
+            elevation_pixel = calculateX(distance_pixel, azimuth)
+            azimuth_pixel = calculateY(distance_pixel, azimuth)
+
+            radius = radiation_w_m2 / 20
+
+            if radius > 50:
+                radius = 50
+
+            draw.ellipse((center_x - radius - elevation_pixel, center_y - radius + azimuth_pixel,
+                          center_x + radius - elevation_pixel, center_y + radius + azimuth_pixel), fill=128)
+
+            radius = 3
+            draw.ellipse((center_x - radius - elevation_pixel, center_y - radius + azimuth_pixel,
+                          center_x + radius - elevation_pixel, center_y + radius + azimuth_pixel), fill=256)
+
+            draw.text((center_x - elevation_pixel + 10, center_y + azimuth_pixel - 16), sample_time.strftime('%H:%M %Z'), (255, 255, 255), font=font)
+
+im.save("output.png")
